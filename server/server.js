43e805fd03cc8034b76c44087ce971d9c47f7193
parth2903 <parthkansal2002@gmail.com>
2024-10-23 16:50:00 +0530

    backend setup

--- server/server.js
+++ server/server.js
@@ -1,35 +1,26 @@
-const express = require('express');
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
-mongoose
-  .connect("mongodb+srv://parthkansal2002:Vinkansal123@cluster0.9eqw0.mongodb.net/")
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
+dotenv.config();
+
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "DELETE", "PUT"],
-      allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "Cache-Control",
-        "Expires",
-        "Pragma",
-      ],
-      credentials: true,
-    })
-);
-
-app.use(cookieParser());
+//middelwares
 app.use(express.json());
+app.use(morgan("dev"));
 
 
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+  });
 
-// Start the server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
